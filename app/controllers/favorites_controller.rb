cf0809d6415112807b9805808fe041e7e9909181
Jobz9999 <238158993+Jobz9999@users.noreply.github.com>
2025-12-23 18:14:18 +0900

    favorite→複数形に変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,4 @@
-class FavoriteController < ApplicationController
+class FavoritesController < ApplicationController
     def create
         post = Post.find(params[:post_id])
         favorite = current_user.favorites.new(post_id: post.id)
